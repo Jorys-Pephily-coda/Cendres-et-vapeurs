ba16d71010b2a7c62be0afa603cb5057a23b7df5
Para <nathan.para@coda-student.school>
2026-02-11 21:54:27 +0100

    le commerce

--- front_fin/front/src/pages/Commerce.tsx
+++ front_fin/front/src/pages/Commerce.tsx
@@ -1,7 +1,36 @@
+import { useEffect, useState } from "react"
+import { fetchCommerce } from "../service/Commerce"
+import '../styles/Commerce.css'
+
 function Commerce() {
+    const [commerceData, setCommerceData] = useState<any[]>([])
+    useEffect(() => {
+        const getCommerceData = async () => {
+            const data = await fetchCommerce()
+            if (data) {
+                setCommerceData(data)
+            }
+            else{
+                console.error('No commerce data received')
+            }
+        }
+        getCommerceData()
+    }, [])
+    
+
     return (
         <div className="commerce">
-            <h1>Commerce</h1>
+            <div className="shop">
+                {commerceData.length > 0 ? (
+                    <ul>
+                        {commerceData.map((item, index) => (
+                            <li key={index}>{item.name} - {item.description}</li>
+                        ))}
+                    </ul>
+                    ) : (
+                    <p>Ya pas de panneau</p>
+                )}
+            </div>
         </div>
     )
 }
