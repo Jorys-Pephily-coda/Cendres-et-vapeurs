23731430b276e3809fd7bedc0a917c26025e5cef
Para <nathan.para@coda-student.school>
2026-02-11 15:04:33 +0100

    le back

--- back_fin/apps/utils/management/commands/seed_db.py
+++ back_fin/apps/utils/management/commands/seed_db.py
@@ -1,4 +1,4 @@
-Django management command to seed the database with sample data
+# Django management command to seed the database with sample data
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 from apps.products.models import Product, ProductVote
