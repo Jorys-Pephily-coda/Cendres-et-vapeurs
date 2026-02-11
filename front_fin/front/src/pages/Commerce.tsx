6fe410490fac14dc53600717311b8cb52fa7abac
Pathan-Nara <nathan.para@coda-student.school>
2026-02-11 22:35:02 +0100

    ouais le magasin + decal le style

--- front_fin/front/src/pages/Commerce.tsx
+++ front_fin/front/src/pages/Commerce.tsx
@@ -3,28 +3,42 @@ import { fetchCommerce } from "../service/Commerce"
 import '../styles/Commerce.css'
 
 function Commerce() {
-    const [commerceData, setCommerceData] = useState<any[]>([])
+    const [commerceData, setCommerceData] = useState<any>([])
+    const [loading, setLoading] = useState(true)
     useEffect(() => {
         const getCommerceData = async () => {
             const data = await fetchCommerce()
             if (data) {
                 setCommerceData(data)
+                setLoading(false)
             }
             else{
                 console.error('No commerce data received')
             }
         }
         getCommerceData()
+        console.log('Commerce data:', commerceData)
     }, [])
+
+    if (loading) {
+        return <div>Loading...</div>
+    }
     
 
     return (
         <div className="commerce">
+            <h1>Bienvenue dans la forge des nains</h1>
+            <p>Fait toi plaisir petit humain mais eh faut que ton porte monnaie tienne gamin</p>
+            <button className="bourse-btn">Cours de la bourse</button>
             <div className="shop">
-                {commerceData.length > 0 ? (
+                {commerceData.count > 0 ? (
                     <ul>
-                        {commerceData.map((item, index) => (
-                            <li key={index}>{item.name} - {item.description}</li>
+                        {commerceData.results.map((product: any) => (
+                            <li className="product-card" key={product.id}>
+                                <h3>{product.name}</h3>
+                                <p>{product.description}</p>
+                                <p>Price: ${product.price}</p>
+                            </li>
                         ))}
                     </ul>
                     ) : (
